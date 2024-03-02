4ef5373fa6e91f29d780c99cd63557f076ea6836
정현우 <75844701+hyeonwoody@users.noreply.github.com>
2024-03-02 14:01:26 +0900

    💚CI : Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 51730,
+  },
   plugins: [react()],
   base: "/BubblePop/",
 })
+
